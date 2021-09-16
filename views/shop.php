0f5c6e9b87718542ae251b28dcf8e9aac0bf1fab
jenna-mae <80732297+jenna-mae@users.noreply.github.com>
2021-09-16 11:31:01 -0700

    fixed footer bug on shop page

--- views/shop.php
+++ views/shop.php
@@ -1,5 +1,5 @@
 <div class="content" id="shop">
-    <div class="wrapper">
+    <div class="wrapper shopProducts">
         <header>
             <?php
                 if(isset($_GET["addedToCart"])) {
@@ -45,70 +45,72 @@
                     </ul>
                 </div>
             </div>
-            <div class="sort">
-                <h2>Sort</h2>
-                <?php
-                    if(isset($_GET["typeId"])) {
-                        $filter = "&typeId=".$_GET["typeId"];
-                    } else if(isset($_GET["catId"])){
-                        $filter = "&catId=".$_GET["catId"];
-                    } else {
-                        $filter = "";
-                    }
-                ?>
-                <form action="index.php?controller=pages&action=shop<?=$filter?>" method="post" id="sortForm">
-                    <select name="sortBy" id="sort">
+            <div class="rightSection">
+                <div class="sort">
+                    <h2>Sort</h2>
                     <?php
-                        $sortOptions = AllProducts::sortOptions();
-                        foreach($sortOptions as $value=>$option) {
-                            $selected = ($value == $_POST["sortBy"]) ? "selected" : "";
-                            ?>
-                            <option value="<?=$value?>" <?=$selected?>><?=$option?></option>
-                            <?php
+                        if(isset($_GET["typeId"])) {
+                            $filter = "&typeId=".$_GET["typeId"];
+                        } else if(isset($_GET["catId"])){
+                            $filter = "&catId=".$_GET["catId"];
+                        } else {
+                            $filter = "";
                         }
                     ?>
-                    </select>
-                </form>
-            </div>
-            <div class="products" id="allProducts">
-                
-                <?php
-                    foreach($this->state["products"] as $product) {
-                ?>
-                <div class="product">
-                    <div class="productWrapper">
-                        <div class="productImage">
-                            <img src="imgs/<?=$product->image?>" alt="Sweeteats Dessert">
-                            <a href="index.php?controller=pages&action=productDetails&productId=<?=$product->id?>" class="gotoItem"></a>
-                        </div>
-                        <div class="productDetails">
-                            <div class="productFlex">
-                                <div class="productLabels">
-                                    <h1><a href="index.php?controller=pages&action=productDetails&productId=<?=$product->id?>"><?=$product->name?></a></h1>
-                                    <p class="categories">
-                                    <?php
-                                        foreach(Product::getCategories($product->id) as $category){
-                                            ?>
-                                            <?=$category?> <span> | </span>
-                                            <?php
-                                        }
-                                    ?>
-                                    </p>
-                                </div>
-                                <div class="productPrice">
-                                    <span class="price">$<?=$product->price?></span>
-                                </div>
+                    <form action="index.php?controller=pages&action=shop<?=$filter?>" method="post" id="sortForm">
+                        <select name="sortBy" id="sort">
+                        <?php
+                            $sortOptions = AllProducts::sortOptions();
+                            foreach($sortOptions as $value=>$option) {
+                                $selected = ($value == $_POST["sortBy"]) ? "selected" : "";
+                                ?>
+                                <option value="<?=$value?>" <?=$selected?>><?=$option?></option>
+                                <?php
+                            }
+                        ?>
+                        </select>
+                    </form>
+                </div>
+                <div class="products" id="allProducts">
+                    
+                    <?php
+                        foreach($this->state["products"] as $product) {
+                    ?>
+                    <div class="product">
+                        <div class="productWrapper">
+                            <div class="productImage">
+                                <img src="imgs/<?=$product->image?>" alt="Sweeteats Dessert">
+                                <a href="index.php?controller=pages&action=productDetails&productId=<?=$product->id?>" class="gotoItem"></a>
                             </div>
-                            <div class="addToCart">
-                                <a href="index.php?controller=cart&action=add&productId=<?=$product->id?>" class="cartAdd hide-sm">Buy</a>
+                            <div class="productDetails">
+                                <div class="productFlex">
+                                    <div class="productLabels">
+                                        <h1><a href="index.php?controller=pages&action=productDetails&productId=<?=$product->id?>"><?=$product->name?></a></h1>
+                                        <p class="categories">
+                                        <?php
+                                            foreach(Product::getCategories($product->id) as $category){
+                                                ?>
+                                                <?=$category?> <span> | </span>
+                                                <?php
+                                            }
+                                        ?>
+                                        </p>
+                                    </div>
+                                    <div class="productPrice">
+                                        <span class="price">$<?=$product->price?></span>
+                                    </div>
+                                </div>
+                                <div class="addToCart">
+                                    <a href="index.php?controller=cart&action=add&productId=<?=$product->id?>" class="cartAdd hide-sm">Buy</a>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div class="productGhost"></div>
+                    <?php
+                        }
+                    ?>
                 </div>
-                <div class="productGhost"></div>
-                <?php
-                    }
-                ?>
             </div>
         </div>
     </div>
